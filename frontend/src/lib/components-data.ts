31f567e3ea87ec3fe900e0fa8b7d74036bd390a4
Yafiet <yafietdaniale@gmail.com>
2024-03-23 00:47:43 +0300

    code politioning and feature addition

---
+++ frontend/src/lib/components-data.ts
@@ -0,0 +1,10 @@
+export const componentsData = [
+    { id: '1', className: 'bi bi-menu-button-wide-fill', text: 'Button', tags: [ 'Button', 'btn', 'button', 'b', 'click']},
+    { id: '2', className: 'bi bi-type-h1', text: 'Heading 1', tags: [ 'Heading 1', 'h1', 'heading 1', 'heading']},
+    { id: '3', className: 'bi bi-type-h2', text: 'Heading 2', tags: [ 'Heading 2', 'h2', 'heading 2', 'sub title', 'secondary title']},
+    { id: '4', className: 'bi bi-type-h3', text: 'Heading 3', tags: [ 'Heading 3', 'h3', 'heading 3', 'third title', 'tertiary title' ]},
+    { id: '5', className: 'bi bi-type-h4', text: 'Heading 4', tags: ['Heading 4', 'h4', 'heading 4', 'fourth title' ]},
+    { id: '6', className: 'bi bi-type-h5', text: 'Heading 5', tags: ['Heading 5', 'h5', 'heading 5', 'fifth title' ]},
+    { id: '7', className: 'bi bi-type-h6', text: 'Heading 6', tags: ['Heading 6', 'h6', 'heading 6', 'sixth title' ]},
+    { id: '8', className: 'bi bi-text-paragraph', text: 'Paragraph', tags: ['Paragraph', 'paragraph', 'p', 'P' ] }
+];
--- frontend/src/modules/Elem.ts
+++ frontend/src/modules/Elem.ts
@@ -1,11 +1,33 @@
+import { PAGES_DATATYPE } from "../id-storage/constants.config";
 
 export default class Elem {
+
+    static isPage(id: string){
+        return this.id(id)!.getAttribute('data-type') === PAGES_DATATYPE;
+    }
 
     static id(id: string){
         if(document.getElementById(id)){
             return document.getElementById(id);
         }else {
             return document.getElementById('canvas')
+        }
+    }
+    
+    static setAttribute(id: string, key: string, value: string){
+        if(Elem.id(id)){
+            Elem.id(id)!.setAttribute(key, value);
+        }else {
+            return false
+        }
+    }
+
+    // to set multiple attributes for element
+    static setMAttribute(id: string, kv: any){
+        if(Elem.id(id)){
+            Object.keys(kv).forEach((key: string) => {
+                Elem.id(id)!.setAttribute(key, kv[key]);
+            })
         }
     }
 
