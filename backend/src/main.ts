9c18642119ff23d5c11728469ee626a4b34646b3
Yafiet <yafietdaniale@gmail.com>
2024-03-25 10:15:23 +0300

    initializing the backend codes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,41 @@
 import express, { Express } from 'express';
+import MainTest from './main.test';
+import cors from 'cors';
+import { env } from 'process';
+import dotenv from 'dotenv';
+// initialization
+dotenv.config(); // dot env config before every thing
+const app: Express = express(); // express app instance
+const { DEV_PROTOCOL,
+        DEV_PORT,
+        DEV_HOST,
+        PRO_PROTOCOL,
+        PRO_PORT,
+        PRO_HOST 
+      } = env; // our environment variables
+const ENVIRONMENT = process.argv[2] ? process.argv[2] : 'UNKNOWN';
 
-const app: Express = express();
+// checking for every test validations
+const isOperational = new MainTest().start();
+isOperational ? console.log('✅ All Tests Passed') : console.log('❌ Test Failed');
+isOperational ? console.log("🔃 Running App") : console.log("🚫 Stopping App");
+isOperational ? console.log('--------------------') : process.exit(1);
 
 
 
-app.get("/", 'welcome to api.hammer.app');
+// environment check
+console.log("- checking environment...")
+if(ENVIRONMENT !== "PRO" && ENVIRONMENT !== "DEV"){
+    console.log('ENVIRONMENT: UNKNOWN')
+    console.log('PLEASE SET ENVIRONMENT:')
+    console.log('tsx ./src/main.ts [ENVIRONMENT_VALUE]')
+    console.log('ENVIRONMENT: PRO [FOR PRODUCTION] DEV [FOR DEVELOPMENT]')
+    process.exit(1)
+}else {
+    console.log('ENVIRONMENT: ' + ENVIRONMENT);
+    console.log('RUNNING: ' + ENVIRONMENT)
+}
+
+
+
+// If environment checks passed codes below run
